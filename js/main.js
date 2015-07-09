ff16434c76d291f06b47f5c4eb02248646d22565
Josh Fabian <contact@joshfabian.com>
2015-07-09 05:34:44 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+jQuery('#register_form').modal('show').on('hide.bs.modal', function (e) {
+  e.preventDefault();
+})
+
+jQuery('#login_form').modal('show').on('hide.bs.modal', function (e) {
+  e.preventDefault();
+})
+
+jQuery('#create_listing').modal('show').on('hide.bs.modal', function (e) {
+  e.preventDefault();
+})
+
+jQuery('#listing_modal').modal('show').on('hide.bs.modal', function (e) {
+  e.preventDefault();
+})
+
+jQuery('#listing_modal2').modal('show').on('hide.bs.modal', function (e) {
+  e.preventDefault();
+})
